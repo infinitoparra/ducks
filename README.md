24fc4d8cf76d627ef66b72b125cd361ebc7fb1e1
fer <fer@ferqwerty.com>
2019-09-26 17:28:49 +0200

    removed theme for serving bss index

--- README.md
+++
@@ -1,5 +0,0 @@
-Bootstrap 4 on Wordpress
-===========
-Bootstrap 4 on WordPress is a blank bootstrap 4 ready WordPress theme.
-
-For the full documentation visit the [Bootstrap 4 on WordPress](http://bootstrap4onwordpress.babobski.nl/) website.
--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form class="form-inline" id="searchform" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-	<input class="form-control" id="s" name="s" type="text" placeholder="<?php echo __('Search', 'wp_babobski'); ?>">
-	<button class="btn btn-outline-success" type="submit">
-		<?php echo __('Search', 'wp_babobski'); ?>
-	</button>
-</form>
